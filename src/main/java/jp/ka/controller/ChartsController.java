1d98b715ee83fcce7dff99f8886cbc4e355c19a6
kurodaakira <kurodaakira.jp@gmail.com>
2021-03-12 09:56:04 +0800

    列表增加序号

--- src/main/java/jp/ka/controller/ChartsController.java
+++ src/main/java/jp/ka/controller/ChartsController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
-import java.util.Map;
 
 @Controller
 public class ChartsController {
